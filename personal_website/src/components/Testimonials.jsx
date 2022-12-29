35e9052d8f5849ae27efe555b8d3d61c3853493e
Mazin Al-Ani <mazinal-ani@Mazins-MacBook-Pro.local>
2022-12-28 22:14:14 -0500

    Card Deals

--- personal_website/src/components/Testimonials.jsx
+++ personal_website/src/components/Testimonials.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import { feedback } from '../constants';
+import styles from '../style';
+import FeedbackCard from './FeedbackCard';
 
-const Testimonials = () => {
-  return (
-    <div>Testimonials</div>
-  )
-}
+const Testimonials = () =>(
+  <section id="clients" className={`${styles.paddingY} ${styles.flexCenter} flex.-col relative`}>
+    {/* TODO */}
+    <div />
+
+    <div></div>
+
+  </section>
+)
 
 export default Testimonials
